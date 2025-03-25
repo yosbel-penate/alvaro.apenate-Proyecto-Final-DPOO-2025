e0d271a9ef9cd6561a1675ee9e3e9d83c2d7ed64
AlvaroPt2004 <alvaropenate349@gmail.com>
2025-03-25 07:02:31 -0600

    Mas Clases
    Clase caracter y clase posicion

--- src/src/Juego.java
+++ src/src/Juego.java
@@ -1,9 +1,28 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+
+
 public class Juego {
     public static void main(String[] args) {
 
         String nombre;
         String descripcion;
         double duracion;
+
+
+        JFrame ventana = new JFrame("Eternal Nights");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(800, 600);
+        ventana.setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        ventana.add(panel);
+
+        ventana.setLocationRelativeTo(null);
+
+        ventana.setVisible(true);
 
 
 
