e0b0f3624225f1c56ee6b9de9ed63c0b60dcb2ba
AlvaroPt2004 <alvaropenate349@gmail.com>
2025-04-13 11:32:40 -0600

    .
    .

--- src/gui/Vista.java
+++ src/gui/Vista.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 public class Vista extends JFrame {
 
     public Vista (){
+        setTitle("Eternal Nights");
         this.setSize(800,600);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
