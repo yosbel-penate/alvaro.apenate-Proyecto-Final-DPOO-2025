0910b03473c6f01bc06ae15f13645bb7c71b0c9e
AlvaroPt2004 <alvaropenate349@gmail.com>
2025-04-13 11:23:07 -0600

    .
    .

--- src/fuente/Juego.java
+++ src/fuente/Juego.java
@@ -1,20 +1,15 @@
 
 package src.fuente;
 
+import src.gui.Vista;
+
 import javax.swing.JFrame;
 
 public class Juego {
 
     public static void main(String[] args) {
        
-        JFrame ventana = new JFrame("Juego");
-        ventana.setVisible(true);
-        ventana.setSize(900, 600);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
-        
+        Vista v1 = new Vista();
+
     }
-    
-    
 }
--- src/gui/Vista.java
+++ src/gui/Vista.java
@@ -5,62 +5,10 @@ import javax.swing.*;
 
 public class Vista extends JFrame {
 
-    public Vista() {
-      
-        initComponents();
-        
-        
+    public Vista (){
+        this.setSize(800,600);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    @SuppressWarnings("unchecked")
-    private void initComponents() {
-
-        jl_Fondo = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jl_Fondo.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        jl_Fondo.setText("      Eternal Night");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jl_Fondo, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jl_Fondo, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 134, Short.MAX_VALUE))
-        );
-
-        pack();
-    }
-    public static void main(String args[]) {
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Vista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Vista().setVisible(true);
-            }
-        });
-    }
-
-    private javax.swing.JLabel jl_Fondo;
 
 }
